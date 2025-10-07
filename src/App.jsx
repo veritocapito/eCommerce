42f3c55961ec502973345a51b5b1db10d305dc79
Veronica Capobianco <veritocapito@gmail.com>
2025-10-07 12:16:04 -0300

    add vercel requirement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -18,6 +19,7 @@ function App() {
           <Route path="/detail/:id" element={<ItemDetailContainer />} />
         </Routes>
         <Footer />
+        <SpeedInsights />
       </BrowserRouter>
     </>
   );
