fcae4248a1d643e357b29f589ed69af0394a94dc
Veronica Capobianco <veritocapito@gmail.com>
2025-11-03 13:26:27 -0300

    update CartProvider functionality

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -10,11 +10,30 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = (item) => {
     if (itemExists(item.id)) {
-      alert("El item ya fue agregado al carrito");
-      return;
+      const updatedCart = cart.map((cartItem) => {
+        if (cartItem.id === item.id) {
+          return { ...cartItem, quantity: cartItem.quantity + item.quantity };
+        } else {
+          return cartItem;
+        }
+      });
+
+      setCart(updatedCart);
+      alert(`${item.name} agregado!`);
+    } else {
+      setCart([...cart, item]);
+      alert(`${item.name} agregado!`);
     }
-    const newItem = { ...item };
-    setCart([...cart, newItem]);
+  };
+
+  const deleteItem = (id) => {
+    const updatedCart = cart.filter((item) => item.id !== id);
+    setCart(updatedCart);
+  };
+
+  const getTotal = () => {
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    return Math.round(total * 100) / 100;
   };
 
   const clearCart = () => {
@@ -22,13 +41,16 @@ export const CartProvider = ({ children }) => {
   };
 
   const getTotalItems = () => {
-    if (cart.length) {
-      return cart.length;
-    }
+    const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+    return totalItems;
+  };
+
+  const checkout = () => {
+    confirm("¿Desea finalizar su compra?") && clearCart();
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, clearCart, getTotalItems }}>
+    <CartContext.Provider value={{ cart, addToCart, deleteItem, clearCart, getTotalItems, getTotal, checkout }}>
       {children}
     </CartContext.Provider>
   );
